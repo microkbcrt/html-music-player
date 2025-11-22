ad1df760281c97caeb09cbc513a890c641944ae5
microkbcrt <1811838153@qq.com>
2025-11-23 01:02:52 +0800

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,15 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// 注意：Preload 脚本中不要 require 'jsmediatags'
-// 我们通过 ipcRenderer.invoke 让主进程去干脏活累活
-
 contextBridge.exposeInMainWorld('electronAPI', {
-  // 监听主进程发来的播放列表
+  // 监听播放列表初始化
   onInitPlaylist: (callback) => ipcRenderer.on('app-init-playlist', callback),
   
-  // 请求读取文件基础数据 (URL, 歌词)
+  // 读取文件内容 (Audio Src, Lyric Text)
   readFileData: (filePath) => ipcRenderer.invoke('read-file-data', filePath),
   
-  // 请求读取音乐 Tags (现在改为调用主进程)
+  // 读取 ID3 Tags (通过主进程调用 jsmediatags)
   readTags: (filePath) => ipcRenderer.invoke('read-music-tags', filePath)
 });
