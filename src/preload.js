cbe7cac3028f32fe6d52154f0bab03f5d03cf26b
microkbcrt <1811838153@qq.com>
2025-11-23 00:51:55 +0800

    Refactor preload.js to use main process for tags

--- src/preload.js
+++ src/preload.js
@@ -1,23 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const jsmediatags = require('jsmediatags');
+
+// 注意：Preload 脚本中不要 require 'jsmediatags'
+// 我们通过 ipcRenderer.invoke 让主进程去干脏活累活
 
 contextBridge.exposeInMainWorld('electronAPI', {
   // 监听主进程发来的播放列表
   onInitPlaylist: (callback) => ipcRenderer.on('app-init-playlist', callback),
   
-  // 请求读取文件详细数据
+  // 请求读取文件基础数据 (URL, 歌词)
   readFileData: (filePath) => ipcRenderer.invoke('read-file-data', filePath),
   
-  // 暴露 jsmediatags 给前端使用 (因为它是纯JS库，可以在浏览器环境跑，但这里我们需要读取本地文件Buffer)
-  // 这里的技巧是：让前端传 file path 进来，我们在 Node 环境读取 tags
-  readTags: (filePath) => {
-    return new Promise((resolve, reject) => {
-      new jsmediatags.Reader(filePath)
-        .setTagsToRead(["title", "artist", "album", "picture"])
-        .read({
-          onSuccess: (tag) => resolve(tag),
-          onError: (error) => reject(error)
-        });
-    });
-  }
+  // 请求读取音乐 Tags (现在改为调用主进程)
+  readTags: (filePath) => ipcRenderer.invoke('read-music-tags', filePath)
 });
